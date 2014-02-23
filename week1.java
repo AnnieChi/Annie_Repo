6b7a1bcbef31117860fc3d6776206c04dc974749
AnnieChi <chu_9_ban@yahoo.com.tw>
2014-02-23 18:46:36 +0800

    0223 新增七言絕句

---
+++ week1.java
@@ -0,0 +1,10 @@
+class Qoo {
+  public static void main(String args[])  { 
+  
+	System.out.println("赤壁 -- 唐．杜牧\n");
+	System.out.println("====================================\n");	
+	System.out.println("折戟沈沙鐵未銷，自將磨洗認前朝。\n");
+	System.out.println("東風不與周郎便，銅雀春深鎖二喬。\n");
+	System.out.println("====================================\n");
+  }
+}
